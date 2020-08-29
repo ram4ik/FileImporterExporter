2daafe8c421d7169f601aaefb6de8acc0a0b0d35
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-29 13:26:25 +0300

    Initial Commit

---
+++ FileImporterExporter/FileImporterExporterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FileImporterExporterApp.swift
+//  FileImporterExporter
+//
+//  Created by Ramill Ibragimov on 29.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct FileImporterExporterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
