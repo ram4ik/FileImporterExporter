29da698c207562d50efe9051a2793e9b589bffca
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-29 13:59:18 +0300

    init

--- FileImporterExporter/ContentView.swift
+++ FileImporterExporter/ContentView.swift
@@ -6,11 +6,66 @@
 //
 
 import SwiftUI
+import UniformTypeIdentifiers
 
 struct ContentView: View {
+    @State private var fileName = ""
+    @State private var openFile = false
+    @State private var saveFile = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing: 25) {
+            
+            Text(fileName)
+                .fontWeight(.bold)
+            
+            Button(action: {openFile.toggle()}, label: {
+                Text("Open")
+            })
+            Button(action: {saveFile.toggle()}, label: {
+                Text("Save")
+            })
+        }
+        .fileImporter(isPresented: $openFile, allowedContentTypes: [.pdf]) { (res) in
+            do {
+                let fileUrl = try res.get()
+                print(fileUrl)
+                
+                self.fileName = fileUrl.lastPathComponent
+            } catch {
+                print("Error reading docs")
+                print(error.localizedDescription)
+            }
+        }
+        .fileExporter(isPresented: $saveFile, document: Doc(url: Bundle.main.path(forResource: "woman", ofType: "png")!), contentType: .audio) { (res) in
+            do {
+                let fileUrl = try res.get()
+                print(fileUrl)
+            }
+            catch {
+                print("cannot save doc")
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
+
+struct Doc: FileDocument {
+    var url: String
+    static var readableContentTypes: [UTType]{[.audio]}
+    
+    init(url: String) {
+        self.url = url
+    }
+    
+    init(configuration: ReadConfiguration) throws {
+        url = ""
+    }
+    
+    func fileWrapper(configuration: WriteConfiguration) throws -> FileWrapper {
+        let file = try! FileWrapper(url: URL(fileURLWithPath: url), options: .immediate)
+        
+        return file
     }
 }
 
